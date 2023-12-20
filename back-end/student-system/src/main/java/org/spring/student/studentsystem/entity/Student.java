3ddb7a3c184143bbdf64257948d47803652bb81f
AhmedElhdad <ahmedelhdad956@gmail.com>
2023-12-20 20:56:04 +0200

    Add annotation on entity Student Closes #2

--- back-end/student-system/src/main/java/org/spring/student/studentsystem/entity/Student.java
+++ back-end/student-system/src/main/java/org/spring/student/studentsystem/entity/Student.java
@@ -1,17 +1,44 @@
 package org.spring.student.studentsystem.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.spring.student.studentsystem.entity.enums.Gender;
 
-import javax.xml.crypto.Data;
+import java.util.Date;
 
+
+@Entity
+@Table(name = "student")
+@Data
 public class Student {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "full_name")
     private String fullName;
+
+    @Column(name = "age")
     private String age;
+
+    @Column(name = "gender")
     private Gender gender;
+
+    @Column(name = "phone")
     private String phone;
+
+    @Column(name = "address")
     private String address;
-    private Data dataCreate;
-    private Data dataUpdated;
+
+    @CreationTimestamp
+    @Column(name = "date_create")
+    private Date dateCreate;
+
+    @UpdateTimestamp
+    @Column(name = "date_update")
+    private Date dateUpdated;
 
 }
