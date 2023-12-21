4e2b46f42d9d50196f9aaa9cd5cac25a8e603e89
AhmedElhdad <ahmedelhdad956@gmail.com>
2023-12-21 20:26:23 +0200

    Update Lombok Closes #6

--- back-end/student-system/src/main/java/org/spring/student/studentsystem/entity/Student.java
+++ back-end/student-system/src/main/java/org/spring/student/studentsystem/entity/Student.java
@@ -1,7 +1,9 @@
 package org.spring.student.studentsystem.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.spring.student.studentsystem.entity.enums.Gender;
@@ -12,6 +14,8 @@ import java.util.Date;
 @Entity
 @Table(name = "student")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
