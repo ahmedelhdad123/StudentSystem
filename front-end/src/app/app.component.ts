2834cf4245c695ac6c290b322db52f24390d797d
AhmedElhdad <ahmedelhdad956@gmail.com>
2023-12-21 23:06:07 +0200

    Second version Frontend Closes #8

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./componants/header/header.component";
+import {FooterComponent} from "./componants/footer/footer.component";
+import {RegisterComponent} from "./componants/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
